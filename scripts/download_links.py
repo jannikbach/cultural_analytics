7c6723bdaf5be631484c328522e8d40f6ceee620
j-hiese <jonas@salzalaeufer.de>
2025-03-08 22:03:29 +0100

    load artists with different styles

--- scripts/download_links.py
+++ scripts/download_links.py
@@ -2,9 +2,12 @@ import random
 import discogs_client
 import csv
 from pathlib import Path
+import discogs_client.models
 import dotenv
 import os
 from tqdm import tqdm
+import pandas as pd
+from utils import TOP_STYLES
 
 dotenv.load_dotenv()
 
@@ -116,5 +119,73 @@ def load_releases():
                 # Update progress bar after each release
                 pbar.update(1)
 
+def load_releases_with_different_styles():
+    api_key = os.getenv('DISCOGS_API_KEY')
+
+    # Initialize Discogs client
+    d = discogs_client.Client('album-cover-project', user_token=api_key)
+
+    results = d.search(type='artist')
+
+    # Path('.fetched_data').mkdir(parents=True, exist_ok=True)
+
+    # with open('.fetched_data/discogs_releases_different.csv', mode='w', newline='', encoding='utf-8') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(['Id', 'Release Name', 'Artist', 'Genre', 'Subgenres', 'Cover URL'])
+
+    with tqdm(total=results.count, desc='artists loaded') as pbar:
+        df = pd.read_csv('.fetched_data/discogs_releases_different.csv', sep=",", quotechar='"')
+        for i in range(results.pages):
+            for artist in list(results.page(i)):
+                pbar.update(1)
+
+                try:
+                    if df["Artist"].eq(artist.name).any():
+                        continue
+                    releases_to_save = []
+                    # releases = artist.releases
+                    #releases = d.search(type='release', genre='Electronic', artist=artist.name)
+
+                    for year in range(2015, 2025):
+                        for style in TOP_STYLES:
+                            releases = d.search(type='release', genre='Electronic', year=str(year), style=style, artist=artist.name)
+
+                            for j in range(releases.pages):
+                                for release in list(releases.page(j)):
+                                    try:
+                                        # if release.year is None or int(release.year) < 2015:
+                                        #     continue
+
+                                        # if not 'Electronic' in release.genres:
+                                        #     continue
+
+                                        if len(release.styles) != 1:
+                                            continue
+
+                                        # if not release.styles[0] in TOP_STYLES:
+                                        #     continue
+
+                                        releases_to_save.append(release)
+                                    except Exception as e:
+                                        print(f"Error in release {release.title}: {e}")
+
+                    styles = {release.styles[0] for release in releases_to_save}
+
+                    with open('.fetched_data/discogs_releases_different.csv', mode='a', newline='', encoding='utf-8') as file:
+                        writer = csv.writer(file)
+
+                        if len(styles) <= 1:
+                            writer.writerow(['N/A','N/A',artist.name,'N/A','N/A','N/A'])
+                            continue
+                        
+                        for release in releases_to_save:
+                            release_name = release.title
+                            genres = ', '.join(release.genres) if release.genres else 'N/A'
+                            subgenres = ', '.join(release.styles) if release.styles else 'N/A'
+                            cover_url = release.images[0]['uri'] if release.images else 'N/A'
+                            writer.writerow([release.id, release_name, artist.name, genres, subgenres, cover_url])
+                except Exception as e:
+                    print(f"Error in artist {artist.name}: {e}")
+
 if __name__ == "__main__":
-    load_releases(['House', 'Hard House', 'Techno', 'Hard Techno', 'Trance', 'Hard Trance'])
+    load_releases_with_different_styles()
